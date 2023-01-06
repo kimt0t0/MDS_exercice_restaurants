c77a6664cc70fec29db4d9b2921d9ff417d979f4
Kim Robert <99035219+kimt0t0@users.noreply.github.com>
2023-01-06 16:15:09 +0100

    wip - course routes and controller working on course post

---
+++ backend/src/routes/courses/index.js
@@ -0,0 +1,51 @@
+const { getCourses, getCourseById, createCourse, updateCourseById, deleteCourseById } = require('../../controllers/coursesController')
+const router = require('express').Router()
+
+
+router.route('/')
+
+    .get(async (req, res) => {
+        const courses = await getCourses()
+        return res.send(courses)
+    })
+
+    .post(async (req, res) => {
+        try {
+            console.log("course creation...")
+            const courseCreated = await createCourse(req.body, '63b832faa974c4aa33659830')
+            return res.send(courseCreated)
+        } catch(error) {
+            return res.status(500).send(error)
+        }
+    })
+
+    router.route('/:id')
+
+        .get(async (req, res) => {
+            try {
+                const course = await getCourseById(req.params.id)
+                return res.send(course)
+            } catch (error) {
+                return res.status(500).send(error)
+            }
+        })
+
+        .patch(async (req, res) => {
+            try {
+                const courseUpdate = await updateCourseById(req.params.id, req.body)
+                return res.send(courseUpdate)
+            } catch (error) {
+                return res.status(500).send(error)
+            }
+        })
+
+        .delete(async (req, res) => {
+            try {
+                await deleteCourseById(req.params.id)
+                return res.send(`User with id: ${req.params.id} has been deleted`)
+            } catch (error) {
+                return res.status(500).send(error)
+            }
+        })
+
+module.exports = router
