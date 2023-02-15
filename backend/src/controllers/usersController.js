66cf660cc68aa2000f6441762634428bc75dcba3
Kim Robert <99035219+kimt0t0@users.noreply.github.com>
2023-02-15 18:13:24 +0100

    wip - creating user with first name and last name on mongodb works

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,68 +1,59 @@
 const User = require('../data/models/User')
 
-
-
 const createUser = async (user) => {
-    if (!user.email || !user.password) {
-        throw new Error('missing data')
-    }
-    const _user = new User({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        password: user.password,
-        role: user.role
-    })
+  if (!user.email || !user.password) {
+    throw new Error('missing data')
+  }
+  const _user = new User({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    password: user.password,
+    role: user.role
+  })
 
-    const savedUser = await _user.save()
-    console.log('saved user: ', savedUser)
-    const savedUserObject = savedUser.toObject()
-    console.log('user object: ', savedUserObject)
-    delete savedUserObject.password
-    return savedUserObject
+  const savedUser = await _user.save()
+  console.log('saved user: ', savedUser)
+  const savedUserObject = savedUser.toObject()
+  console.log('user object: ', savedUserObject)
+  delete savedUserObject.password
+  return savedUserObject
 }
-
-
 
 const getUsers = async () => {
-    const users = await User.find().select('-password')
-    return users
+  const users = await User.find().select('-password')
+  return users
 }
 
-
-
-const getUserById = async(userId) => {
-    if (!userId) {
-        throw new Error('missing user id')
-    }
-    const _user = await User.findById(userId).select('-password')
-    const userObject = _user.toObject()
-    return userObject
+const getUserById = async (userId) => {
+  if (!userId) {
+    throw new Error('missing user id')
+  }
+  const _user = await User.findById(userId).select('-password')
+  const userObject = _user.toObject()
+  return userObject
 }
-
 
 const updateUserById = async (userId, user) => {
-    if (!userId || ! user) {
-        throw new Error('missing data')
-    }
-    const _user = await User.findByIdAndUpdate(userId, user, {new:true}).select('-password')
-    const userObject = _user.toObject()
-    return userObject
+  if (!userId || !user) {
+    throw new Error('missing data')
+  }
+  const _user = await User.findByIdAndUpdate(userId, user, { new: true }).select('-password')
+  const userObject = _user.toObject()
+  return userObject
 }
-
 
 const deleteUserById = async (userId) => {
-    if (!userId) {
-        throw new Error('missing user id')
-    }
-    await User.findByIdAndDelete(userId)
+  if (!userId) {
+    throw new Error('missing user id')
+  }
+  await User.findByIdAndDelete(userId)
 }
 
-
 module.exports = {
-    createUser,
-    getUsers,
-    getUserById,
-    updateUserById,
-    deleteUserById
+  createUser,
+  getUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById
 }
